e1a55e4a2433364133b0904c492a22f12eaa14fe
Pbhadula <30542866+Pbhadula@users.noreply.github.com>
2022-03-04 12:34:00 +0530

    initial commit

---
+++ src/components/createarea.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+
+function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+      });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+    
+        setNote(prevNote => {
+          return {
+            ...prevNote,
+            [name]: value
+          };
+        });
+      }
+
+      function submitNote (event) {
+        props.onAdd(note);
+        setNote({
+          title: "",
+          content: ""
+        });
+        event.preventDefault();
+      }
+
+      function expand() {
+        setExpanded(true);
+      }
+
+    return (
+        <div>
+            <form className="create-note" action="">
+              {isExpanded && (
+                      <input 
+                      type="text" 
+                      name="title" 
+                      onChange={handleChange} 
+                      value={note.title} 
+                      placeholder="Title" />
+              )
+
+              }
+            
+                <textarea
+                 name="content" 
+                 rows={isExpanded ? 3 : 1}
+                 onClick = {expand} 
+                 onChange={handleChange} 
+                 value={note.content} 
+                 placeholder="Take a note.." />
+
+                <Fab onClick={submitNote}>
+                     <AddIcon />
+                </Fab>
+            </form>
+        </div>
+    )
+}
+
+export default CreateArea;
